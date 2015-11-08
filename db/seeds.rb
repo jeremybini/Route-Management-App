9f3a576b8c57ab388cfa336d934989ab79c94154
jeremybini <jeremybini@gmail.com>
2015-11-07 21:02:42 -0500

    assets precompile

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,3 @@ super_admin = User.create(full_name: "Jeremy Bini", email: "jeremybini@gmail.com
 manager = User.create(full_name: "Boss Person", email: "manager@email.com", password: "manager1234", password_confirmation:"manager1234", auth_token:"123456789", role: "Admin")
 setter = User.create(full_name: "Route Setter", email: "setter@email.com", password: "setter1234", password_confirmation: "setter134", auth_token:"423456789", role: "Routesetter")
 user = User.create(full_name: "Regular Ol' User", email: "user@email.com", password: "user1234", password_confirmation:"user1234", auth_token:"323456789")
-
